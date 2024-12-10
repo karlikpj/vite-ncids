effcfd4738b760baf586d9ca3923b9b408684c5d
karlikpj <paul.karlik@nih.gov>
2024-12-10 07:47:12 -0500

    First Commit

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+import { NodePackageImporter } from 'sass';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	build: {
+		minify: true,
+		cssMinify: true,
+		rollupOptions: {
+		},
+	},
+	css: {
+		preprocessorOptions: {
+			scss: {
+				// quietDeps: true,
+				// includePaths: [
+				// 	path.join(
+				// 		__dirname,
+				// 		'node_modules/@nciocpl/ncids-css/packages',
+				// 	),
+				// 	path.join(
+				// 		__dirname,
+				// 		'node_modules/@nciocpl/ncids-css/uswds-packages',
+				// 	),
+				// ],
+                api: "modern",
+                importers: [new NodePackageImporter()],
+			},
+		},
+	},
+});
