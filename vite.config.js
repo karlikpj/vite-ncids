983c3615d4235896ab658d024857d3f34c791416
karlikpj <paul.karlik@nih.gov>
2024-12-10 08:04:19 -0500

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -13,19 +13,19 @@ export default defineConfig({
 	css: {
 		preprocessorOptions: {
 			scss: {
-				// quietDeps: true,
-				// includePaths: [
-				// 	path.join(
-				// 		__dirname,
-				// 		'node_modules/@nciocpl/ncids-css/packages',
-				// 	),
-				// 	path.join(
-				// 		__dirname,
-				// 		'node_modules/@nciocpl/ncids-css/uswds-packages',
-				// 	),
-				// ],
-                api: "modern",
-                importers: [new NodePackageImporter()],
+				quietDeps: true,
+				includePaths: [
+					path.join(
+						__dirname,
+						'node_modules/@nciocpl/ncids-css/packages',
+					),
+					path.join(
+						__dirname,
+						'node_modules/@nciocpl/ncids-css/uswds-packages',
+					),
+				],
+                //api: "modern",
+                //importers: [new NodePackageImporter()],
 			},
 		},
 	},
